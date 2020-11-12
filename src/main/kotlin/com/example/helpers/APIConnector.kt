9e9443bcdfcb1ce0503abb29a52fcd61986c97ea
Ju5tMyWo4d <ju5tmywo4d@gmail.com>
2020-11-13 00:00:17 +0100

    Add authorization in requests

--- src/main/kotlin/com/example/helpers/APIConnector.kt
+++ src/main/kotlin/com/example/helpers/APIConnector.kt
@@ -20,17 +20,37 @@ class APIConnector {
         )
     ).jsonObject.getJSONObject("payload").getString("token")
 
-    fun authorize(vararg permissions: String): Boolean = post(
+    fun invalidateToken(token: String): Boolean = get(
+        "$API_BASE_URL/token/invalidate",
+        headers = mapOf(
+            "Authorization" to "Bearer $token"
+        )
+    ).statusCode == 200
+
+    fun authorize(token: String, vararg permissions: String): Boolean = post(
         "$API_BASE_URL/authorize", data = JSONObject(
             mapOf(
                 "requiredPermissions" to permissions.toString()
             )
+        ),
+        headers = mapOf(
+            "Authorization" to "Bearer $token"
         )
     ).statusCode == 200
 
-    fun refreshToken(): String = get("$API_BASE_URL/token").jsonObject.getJSONObject("payload").getString("token")
+    fun refreshToken(token: String): String = get(
+        "$API_BASE_URL/token",
+        headers = mapOf(
+            "Authorization" to "Bearer $token"
+        )
+    ).jsonObject.getJSONObject("payload").getString("token")
 
-    fun getUsers(): List<User> = get("$API_CLIENT_URL/users").jsonObject.getJSONArray("payload").map {
+    fun getUsers(token: String): List<User> = get(
+        "$API_CLIENT_URL/users",
+        headers = mapOf(
+            "Authorization" to "Bearer $token"
+        )
+    ).jsonObject.getJSONArray("payload").map {
         it as JSONObject
         User(it.getString("entity"), it.getString("username"))
     }
