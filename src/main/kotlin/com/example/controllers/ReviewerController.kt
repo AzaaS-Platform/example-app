00d9aff60126a4a568c70585f628c5d846888306
Ju5tMyWo4d <sztenk07+github2@gmail.com>
2020-11-19 09:57:59 +0100

    Add permission for articles to review

--- src/main/kotlin/com/example/controllers/ReviewerController.kt
+++ src/main/kotlin/com/example/controllers/ReviewerController.kt
@@ -5,6 +5,7 @@ import com.example.services.DB
 import com.example.services.PermissionsService
 import com.example.services.REVIEWER_ACCEPT_PERMISSION
 import com.example.services.REVIEWER_PERMISSION
+import com.example.services.EDITOR_PERMISSION
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
@@ -15,18 +16,19 @@ import org.springframework.web.bind.annotation.RequestMapping
 @Controller
 @RequestMapping("/reviewer")
 class ReviewerController(
-    private val permissionsService: PermissionsService,
-    private val db: DB
+        private val permissionsService: PermissionsService,
+        private val db: DB
 ) {
 
     @GetMapping("articles")
     fun articles(model: Model): String {
+        permissionsService.enforcePermissions(EDITOR_PERMISSION)
         model["title"] = "Articles to review"
         model["articles"] = db.articles.filter { !it.accepted }
         if (permissionsService.hasPermissions(REVIEWER_PERMISSION)) {
             model["operations"] = listOf(
-                Operation("/editor/articles/delete", "DELETE"),
-                Operation("/reviewer/articles/accept", "ACCEPT")
+                    Operation("/editor/articles/delete", "DELETE"),
+                    Operation("/reviewer/articles/accept", "ACCEPT")
             )
         }
 
